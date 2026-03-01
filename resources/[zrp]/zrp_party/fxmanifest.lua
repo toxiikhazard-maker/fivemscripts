38e19cac0e03c968033842566ae8c5b0f1f83883
toxiikhazard-maker <toxiikhazard@gmail.com>
2026-03-01 23:37:53 +0100

    Add root txAdmin recipe and simplify raw URL usage

---
+++ resources/[zrp]/zrp_party/fxmanifest.lua
@@ -0,0 +1,24 @@
+fx_version 'cerulean'
+game 'gta5'
+
+name 'zrp_party'
+version '1.0.0'
+lua54 'yes'
+
+shared_scripts {
+    '@ox_lib/init.lua'
+}
+
+client_scripts {
+    'client/main.lua'
+}
+
+server_scripts {
+    'server/main.lua'
+}
+
+dependencies {
+    'qb-core',
+    'ox_lib',
+    'zrp_core'
+}
